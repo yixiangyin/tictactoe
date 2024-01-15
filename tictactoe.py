a1c3150facedec260e4d18dc27b8ae3a5c0355ce
yixiangyin <y1027020239@qq.com>
2024-01-15 14:54:11 +0800

    fixed some bugs to run

--- tictactoe.py
+++ tictactoe.py
@@ -10,10 +10,13 @@ EMPTY = None
 
 negative_inf = float('-inf')
 
+DEBUG = 1
 def initial_state():
     """
     Returns starting state of the board.
     """
+    # if DEBUG:
+    #     print("in initial state")
     return [[EMPTY, EMPTY, EMPTY],
             [EMPTY, EMPTY, EMPTY],
             [EMPTY, EMPTY, EMPTY]]
@@ -23,6 +26,8 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
+    if DEBUG:
+        print("in player")
     # count X and O
     x_count = 0
     o_count = 0
@@ -41,6 +46,8 @@ def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
+    if DEBUG:
+        print("in actions")
     actions = []
     for row_idx in range(3):
         for col_idx in range(3):
@@ -53,6 +60,8 @@ def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
+    if DEBUG:
+        print("in result")
     whose_turn = player(board)
     new_board = copy.deepcopy(board)
     new_board[action[0]][action[1]] = whose_turn
@@ -63,6 +72,8 @@ def won_or_no(board, player):
     """
     Return if the given player has won or not on the board
     """
+    if DEBUG:
+        print("in won_or_no")
     # hor
     for row in board:
         if (row[0] == player) and (row[1] == player) and (row[2] == player):
@@ -86,7 +97,7 @@ def won_or_no(board, player):
     # second diagonal
     memo = True
     for i in range(3):
-        for j in range(3, 0, -1):
+        for j in range(2, -1, -1):
             memo = memo and (board[i][j])
     if memo:
         return player
@@ -97,6 +108,8 @@ def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
+    if DEBUG:
+        print("in winner")
     if won_or_no(board, X):
         return X
     if won_or_no(board, O):
@@ -105,6 +118,8 @@ def winner(board):
 
 
 def is_filled(board):
+    if DEBUG:
+        print("in is_filled")
     for i in range(3):
         for j in range(3):
             if board[i][j] is None:
@@ -116,6 +131,8 @@ def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
+    if DEBUG:
+        print("in terminal")
     return winner(board) is not None or is_filled(board)
 
 
@@ -123,10 +140,12 @@ def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    winner = winner(board)
-    if winner == X:
+    if DEBUG:
+        print("in utility")
+    winner_ = winner(board)
+    if winner_ == X:
         return 1
-    elif winner == O:
+    elif winner_ == O:
         return -1
     else:
         return 0
@@ -134,41 +153,47 @@ def utility(board):
 
 def min_value(board):
     """try to minimise the score of the board"""
+    if DEBUG:
+        print("in min_value")
     v = float('inf')
     optimal_action = None
     if terminal(board):
-        return utility(board)
-    actions = actions(board)
-    for action in actions:
-        v = min(v, max_value(result(board, action)))
-        if max_value(result(board, action)) < v:
+        return utility(board), optimal_action
+    actions_ = actions(board)
+    for action in actions_:
+        v = min(v, max_value(result(board, action))[0])
+        if (max_value(result(board, action)))[0] < v:
             optimal_action = action
     return v, optimal_action
 
 
 def max_value(board):
     """try to maximise the score of the board"""
+    if DEBUG:
+        print("in max_value")
     v = negative_inf
     optimal_action = None
     if terminal(board):
-        return utility(board)
-    actions = actions(board)
-    for action in actions:
-        v = max(v, min_value(result(board, action)))
-        if min_value(result(board, action)) > v:
+        return utility(board), optimal_action
+    actions_ = actions(board)
+    for action in actions_:
+        v = max(v, min_value(result(board, action))[0])
+        if (min_value(result(board, action)))[0] > v:
             optimal_action = action
     return v, optimal_action
 
+    
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     X tries to maximise. O tries to minimise
     """
+    if DEBUG:
+        print("in minimax")
     if terminal(board):
         return None
-    player = player(board)
-    actions = actions(board)
-    if player == X:
+    player_ = player(board)
+    if player_ == X:
         return max_value(board)[1]
     return min_value(board)[1]
