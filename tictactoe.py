cb4b9b5b8828e4b4be961ffba2412f720152b68d
yixiangyin <y1027020239@qq.com>
2024-01-17 15:19:03 +0800

    fix bug in if_player_win

--- tictactoe.py
+++ tictactoe.py
@@ -2,7 +2,7 @@
 Tic Tac Toe Player
 """
 import copy
-
+import time
 X = "X"
 O = "O"
 EMPTY = None
@@ -13,7 +13,7 @@ DEBUG = True
 
 def log(s):
     if DEBUG:
-        print(s)
+        print(f"{time.time()}:{s}")
 
 def initial_state():
     """
@@ -28,8 +28,7 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    if DEBUG:
-        print("in player")
+    log("in player")
     # count X and O
     x_count = 0
     o_count = 0
@@ -48,8 +47,7 @@ def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    if DEBUG:
-        print("in actions")
+    log("in actions")
     actions = []
     for row_idx in range(3):
         for col_idx in range(3):
@@ -62,8 +60,7 @@ def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    if DEBUG:
-        print("in result")
+    log("in result")
     whose_turn = player(board)
     new_board = copy.deepcopy(board)
     new_board[action[0]][action[1]] = whose_turn
@@ -74,11 +71,12 @@ def if_player_win(board, player) -> bool:
     """
     Return if the given player has won or not on the board
     """
-    if DEBUG:
-        print("in won_or_no")
+    log("in won_or_no")
+    # log(f"board: {board}")
     # hor
     for row in board:
         if (row[0] == player) and (row[1] == player) and (row[2] == player):
+            log("a row hor")
             return True
     # ver
     for col_idx in range(3):
@@ -86,6 +84,7 @@ def if_player_win(board, player) -> bool:
         for row_idx in range(3):
             memo = memo and (board[row_idx][col_idx] == player)
         if memo:
+            log(f"a row ver on column {col_idx}")
             return True
     # dia
     # first diagonal
@@ -94,14 +93,16 @@ def if_player_win(board, player) -> bool:
     for idx in range(3):
         memo = memo and (board[idx][idx] == player)
     if memo:
+        log("a row dia(first)")
         return True
     # (0,2) (1,1) (2,0)
     # second diagonal
     memo = True
     for i in range(3):
-        for j in range(2, -1, -1):
-            memo = memo and (board[i][j] == player)
+        j = 2 - i
+        memo = memo and (board[i][j] == player)
     if memo:
+        log("a row dia(second)")
         return True
     return False
 
@@ -110,23 +111,20 @@ def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    if DEBUG:
-        print("in winner")
+    log(board)
+    log("in winner")
     if if_player_win(board, X):
-        if DEBUG:
-            print("X won!")
+        log("X won!")
         return X
     if if_player_win(board, O):
-        if DEBUG:
-            print("O won!")
+        log("O won!")
         return O
     
     return None
 
 
 def is_filled(board) -> bool:
-    if DEBUG:
-        print("in is_filled")
+    log("in is_filled")
     for i in range(3):
         for j in range(3):
             if board[i][j] is None:
@@ -138,8 +136,7 @@ def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    if DEBUG:
-        print("in terminal")
+    log("in terminal")
     return winner(board) is not None or is_filled(board)
 
 
@@ -147,8 +144,7 @@ def utility(board) -> int:
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    if DEBUG:
-        print("in utility")
+    log("in utility")
     winner_ = winner(board)
     if winner_ == X:
         return 1
@@ -160,8 +156,7 @@ def utility(board) -> int:
 
 def min_value(board):
     """try to minimise the score of the board"""
-    if DEBUG:
-        print("in min_value")
+    log("in min_value")
     v = float('inf')
     optimal_action = None
     if terminal(board):
@@ -177,8 +172,7 @@ def min_value(board):
 
 def max_value(board):
     """try to maximise the score of the board"""
-    if DEBUG:
-        print("in max_value")
+    log("in max_value")
     v = negative_inf
     optimal_action = None
     if terminal(board):
@@ -197,8 +191,7 @@ def minimax(board):
     Returns the optimal action for the current player on the board.
     X tries to maximise. O tries to minimise
     """
-    if DEBUG:
-        print("in minimax")
+    log(f"in minimax on board: {board}")
     if terminal(board):
         return None
     player_ = player(board)
